20aa33a4b46f6aa218c0ed2ac1a72a9c4e5fe05b
perymerdeka <tkjasikk@gmail.com>
2021-09-21 23:24:53 +0700

    Done Get total Pages Ebay

--- README.md
+++ README.md
@@ -3,5 +3,10 @@ Ebay Scraper for JeparaDev meetup
 
 #### Studi Case Scraping
 * **task**
-  * Parsing Item
+  * Getting Detail of Product in [Ebay.com](https://ebay.com)
+  * getting product detail like 
+    * title
+    * rating
+    * image
+    * price
   * Generate to CSV or Excel Using Pandas
---
+++ main.py
@@ -0,0 +1,60 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers: dict = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'referer': 'https://www.ebay.com/',
+    'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
+}
+
+
+def get_total_pages() -> int:
+    total_pages: list = []
+    params: dict = {
+        '_from': 'R40',
+        '_trksid': 'p2380057.m570.l1313',
+        '_nkw': 'iphone'.replace(' ', '+'),
+        '_sacat': '0',
+    }
+
+    url: str = 'https://www.ebay.com/sch/i.html?'
+    res = requests.get(url=url, params=params, headers=headers)
+    f = open('temp/res.html', 'w+')
+    f.write(res.text)
+    f.close()
+
+    # scraping process
+    soup = BeautifulSoup(res.text, 'html.parser')
+    pagination = soup.find('nav', attrs={'class': 'pagination'})
+    pages = pagination.find_all('li')
+    for item in pages:
+        paginate = item.find('a').text.strip()
+        print(paginate)
+        total_pages.append(int(paginate))
+
+    # get max total Pages
+    total: int = max(total_pages)
+    print(f'Total page found: {total}')
+    return total
+
+
+def get_all_items():
+    pass
+
+
+def get_detail():
+    pass
+
+
+if __name__ == '__main__':
+    get_total_pages()
